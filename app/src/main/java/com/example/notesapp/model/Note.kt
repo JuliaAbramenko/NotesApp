54a2d22af440128565133ae5b9177ce0dacbf14f
Julia Abramenko <julia.abramenko@protonmail.com>
2023-06-01 14:18:11 +0200

    Added kotlin-parcelize to gradle, created ROOM entity class Note, improved folder structure

---
+++ app/src/main/java/com/example/notesapp/model/Note.kt
@@ -0,0 +1,18 @@
+package com.example.notesapp.model
+
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
+
+@Entity(tableName = "notes")
+@Parcelize
+data class Note(
+    @PrimaryKey
+    val id: Int,
+    val noteTitle: String,
+    val noteContent: String
+) : Parcelable // transporting data across intents easily
+{
+
+}
