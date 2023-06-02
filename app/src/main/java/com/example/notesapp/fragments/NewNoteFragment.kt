7708993269ca5aab825255444253dd6641cd8adc
Julia Abramenko <julia.abramenko@protonmail.com>
2023-06-02 08:31:21 +0200

    Added NewNoteFragment and enabled auto-generating ids in note entity

--- app/src/main/java/com/example/notesapp/fragments/NewNoteFragment.kt
+++ app/src/main/java/com/example/notesapp/fragments/NewNoteFragment.kt
@@ -2,19 +2,83 @@ package com.example.notesapp.fragments
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.example.notesapp.MainActivity
 import com.example.notesapp.R
+import com.example.notesapp.adapter.NoteAdapter
+import com.example.notesapp.databinding.FragmentHomeBinding
+import com.example.notesapp.databinding.FragmentNewNoteBinding
+import com.example.notesapp.model.Note
+import com.example.notesapp.viewmodel.NoteViewModel
 
-class NewNoteFragment : Fragment() {
+class NewNoteFragment : Fragment(R.layout.fragment_new_note) {
+
+    private  var _binding : FragmentNewNoteBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var notesViewModel: NoteViewModel
+    private lateinit var noteAdapter: NoteAdapter
+
+    private lateinit var fView: View
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_new_note, container, false)
+        /// Inflate the layout for this fragment with binding
+        _binding = FragmentNewNoteBinding.inflate(inflater, container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        notesViewModel = (activity as MainActivity).notesViewModel
+        fView = view
+    }
+
+    fun saveNote(view: View) {
+        val noteTitle = binding.etNoteTitle.text.toString().trim()
+        val noteBody = binding.etNoteBody.text.toString().trim()
+
+        if(noteTitle.isNotEmpty()) {
+            val note = Note(0, noteTitle, noteBody)
+            notesViewModel.addNote(note)
+            Toast.makeText(fView.context, "Note saved successfully", Toast.LENGTH_LONG).show()
+
+            view.findNavController().navigate(R.id.action_newNoteFragment_to_homeFragment)
+        }
+        else {
+            Toast.makeText(fView.context, "Please enter note title", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        menu.clear()
+        inflater.inflate(R.menu.new_note_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.menuSave -> saveNote(fView)
+        }
+        return super.onContextItemSelected(item)
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
 }
