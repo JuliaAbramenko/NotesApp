09413a752c3aa05537398fb7f80daf1d2b50ec58
Julia Abramenko <julia.abramenko@protonmail.com>
2023-06-02 08:59:35 +0200

    Added setup of view model in MainActivity

--- app/src/main/java/com/example/notesapp/MainActivity.kt
+++ app/src/main/java/com/example/notesapp/MainActivity.kt
@@ -2,13 +2,30 @@ package com.example.notesapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.example.notesapp.database.NoteDatabase
+import com.example.notesapp.databinding.ActivityMainBinding
+import com.example.notesapp.repository.NoteRepository
 import com.example.notesapp.viewmodel.NoteViewModel
+import com.example.notesapp.viewmodel.NoteViewModelFactory
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding
     lateinit var notesViewModel: NoteViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setUpViewModel()
+
+    }
+
+    private fun setUpViewModel() {
+        val noteRepository = NoteRepository(NoteDatabase(this))
+        val viewModelProviderFactory = NoteViewModelFactory(application, noteRepository)
+        notesViewModel =
+            ViewModelProvider(this, viewModelProviderFactory).get(NoteViewModel::class.java)
     }
 }
